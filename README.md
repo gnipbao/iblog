99481756de5adf8756bc4be099555adff1f5f688
gnipbao <gnipbao@gmail.com>
2019-12-05 18:51:21 +0800

    Update blog

--- README.md
+++ README.md
@@ -13,6 +13,16 @@
 ## 文章列表
 
 
+#### 2019-12-05
+[如何使用JavaScript压缩解压zip包](https://github.com/gnipbao/iblog/issues/37)
+#### 2019-09-29
+[JavaScript中Promise在同步代码中的两种执行方式](https://github.com/gnipbao/iblog/issues/36)
+#### 2019-06-17
+[url、base64、blob互转](https://github.com/gnipbao/iblog/issues/35)
+#### 2019-05-21
+[VP9解码器技术方案调研](https://github.com/gnipbao/iblog/issues/34)
+#### 2019-03-30
+[成为 Emacs高手 像神一样使用编辑器](https://github.com/gnipbao/iblog/issues/33)
 #### 2019-01-29
 [ffmpeg常用命令](https://github.com/gnipbao/iblog/issues/32)
 #### 2018-10-02
@@ -24,7 +34,7 @@
 #### 2018-07-21
 [CentOS7中防火墙的一些常用配置介绍](https://github.com/gnipbao/iblog/issues/28)
 #### 2018-06-12
-[video标签在不同平台上差异分析](https://github.com/gnipbao/iblog/issues/27)
+[h5 video在不同平台上差异分析](https://github.com/gnipbao/iblog/issues/27)
 #### 2018-06-08
 [EME WTF？ 加密媒体扩展介绍](https://github.com/gnipbao/iblog/issues/26)
 #### 2018-05-22
@@ -48,9 +58,9 @@
 #### 2017-11-07
 [Uint8Array to string in Javascript](https://github.com/gnipbao/iblog/issues/16)
 #### 2017-10-14
-[vim速查手册](https://github.com/gnipbao/iblog/issues/15)
+[vim速记手册](https://github.com/gnipbao/iblog/issues/15)
 #### 2017-09-14
-[javascript函数式编程](https://github.com/gnipbao/iblog/issues/14)
+[javascript函数式编程--入门篇](https://github.com/gnipbao/iblog/issues/14)
 #### 2017-08-24
 [javascript函数式编程术语](https://github.com/gnipbao/iblog/issues/13)
 #### 2017-05-19
@@ -63,15 +73,5 @@
 [JavaScript闭包以及常见用法](https://github.com/gnipbao/iblog/issues/9)
 #### 2017-04-15
 [JavaScript函数的特性系列目录](https://github.com/gnipbao/iblog/issues/8)
-#### 2017-04-15
-[JavaScript中call,apply,bind以及this的理解](https://github.com/gnipbao/iblog/issues/7)
-#### 2017-04-15
-[JavaScript中常用函数集合](https://github.com/gnipbao/iblog/issues/6)
-#### 2017-04-15
-[JavaScript原生数组](https://github.com/gnipbao/iblog/issues/5)
-#### 2017-04-15
-[Markdown简明语法手册](https://github.com/gnipbao/iblog/issues/4)
-#### 2017-04-15
-[黑客精神](https://github.com/gnipbao/iblog/issues/3)
 ***************
 [⬆ back to top](#文章列表)
--- script/build.js
+++ script/build.js
@@ -1,66 +1,76 @@
-const rp = require('request-promise'),
-  path = require('path'),
-  fs = require('fs-extra'),
-  chalk = require('chalk');
+const rp = require("request-promise"),
+  path = require("path"),
+  fs = require("fs-extra"),
+  chalk = require("chalk");
 // your  github api issues
-const issuesUrl = 'https://api.github.com/repos/gnipbao/iblog/issues';
+const issuesUrl = "https://api.github.com/repos/gnipbao/iblog/issues";
 const options = {
   uri: issuesUrl,
   qs: {
-    access_token: '8275d52f1c77f9da8df09bcf0243e5e56e00314f',// access token
-    sort: 'created'
+    // access_token: '8275d52f1c77f9da8df09bcf0243e5e56e00314f',// access token
+    sort: "created"
   },
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
+    "User-Agent":
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"
   },
   json: true // Automaticlly parse then jsno string in the response
 };
 
-const getIssuesAticles = function (url) {
-  return new Promise(function (resolve, reject) {
-    rp(options).then(function (data) {
-      resolve(data);
-    }).catch(function (err) {
-      reject(err);
-    })
+const getIssuesAticles = function(url) {
+  return new Promise(function(resolve, reject) {
+    rp(options)
+      .then(function(data) {
+        resolve(data);
+      })
+      .catch(function(err) {
+        reject(err);
+      });
   });
 };
 
-const mdTemplatePath = './template';
-let output = '';
+const mdTemplatePath = "./template";
+let output = "";
 // start timer of the script
-console.time('Builder');
+console.time("Builder");
 try {
-  startMdTemplate = fs.readFileSync(path.join(mdTemplatePath, 'start.md'), 'utf-8');
-  endMdTemplate = fs.readFileSync(path.join(mdTemplatePath, 'end.md'), 'utf-8');
+  startMdTemplate = fs.readFileSync(
+    path.join(mdTemplatePath, "start.md"),
+    "utf-8"
+  );
+  endMdTemplate = fs.readFileSync(path.join(mdTemplatePath, "end.md"), "utf-8");
 } catch (error) {
-  console.log(`${chalk.red('Error!')} during static template loading: ${error}`);
+  console.log(
+    `${chalk.red("Error!")} during static template loading: ${error}`
+  );
   process.exit(1);
-};
+}
 
 try {
-  output += `${startMdTemplate+ '\n'}`;
-  getIssuesAticles().then(function (data) {
-    for(let i of data){
-      console.log(`${chalk.green('title')}->${i.title}`);
-      console.log(`${chalk.green('url')}->${i.html_url}`);
-      let createdDate = i.created_at.split('T')[0];
-      console.log(`${chalk.green('created_at')}->${createdDate}`);
-      output += `#### ${createdDate}\n`;
-      output += `[${i.title}](${i.html_url})`;
-      output += `\n`
+  output += `${startMdTemplate + "\n"}`;
+  getIssuesAticles().then(
+    function(data) {
+      for (let i of data) {
+        console.log(`${chalk.green("title")}->${i.title}`);
+        console.log(`${chalk.green("url")}->${i.html_url}`);
+        let createdDate = i.created_at.split("T")[0];
+        console.log(`${chalk.green("created_at")}->${createdDate}`);
+        output += `#### ${createdDate}\n`;
+        output += `[${i.title}](${i.html_url})`;
+        output += `\n`;
+      }
+      output += `${endMdTemplate + "\n"}`;
+      // write to the README file
+      fs.writeFileSync("README.md", output);
+      // log a success message
+      console.log(`${chalk.green("SUCCESS!")} README file generated!`);
+      console.timeEnd("Builder");
+    },
+    function(err) {
+      console.log(`${chalk.red("Error:")}->${err}`);
     }
-    output += `${endMdTemplate+ '\n'}`
-    // write to the README file 
-    fs.writeFileSync('README.md', output);
-    // log a success message
-    console.log(`${chalk.green('SUCCESS!')} README file generated!`);
-    console.timeEnd('Builder');
-
-  }, function (err) {
-    console.log(`${chalk.red('Error:')}->${err}`);
-  });
+  );
 } catch (error) {
-  console.log(`${chalk.red('Error:')}->${error}`);
+  console.log(`${chalk.red("Error:")}->${error}`);
   process.exit(1);
 }
